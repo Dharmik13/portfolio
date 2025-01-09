2bae1795a15d42841798ab92ca085dbdd9127cd7
Dharmik Mukeshbhai Patel <88429448+Dharmik13@users.noreply.github.com>
2025-01-09 12:59:03 +0530

    add resume in my website

--- src/components/Education And Experience/EduExpHeader.js
+++ src/components/Education And Experience/EduExpHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EduExp from "./EduExp";
 import "../Education And Experience/eduexp.css";
+import BlackLine from "../Black Line/BlackLine";
 
 const EduExpHeader = () => {
   return (
@@ -10,6 +11,25 @@ const EduExpHeader = () => {
         <div className="heading">Education And Experience</div>
       </div>
       <EduExp />
+
+      <div className="header">
+        <div className="headDesc">Download or View My Resume</div>
+        <div className="heading">My Resume</div>
+      </div>
+      <div className="resumeButton">
+        <button
+          onClick={() =>
+            window.open(
+              "https://drive.google.com/file/d/1KSAuBY2JBKqwHgstZjbOYhbQ-d8ebVSC/view",
+              "_blank"
+            )
+          }
+          className="btn"
+        >
+          Download or View My Resume
+        </button>
+      </div>
+      <BlackLine />
     </section>
   );
 };
